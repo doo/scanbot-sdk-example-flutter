329d624908eeec1f3b4252d5a2409e9139e33848
Serhii Chaban <serhii@scanbot.io>
2023-01-03 17:50:32 +0100

    classical component cropping example widget

--- lib/ui/operations_page_widget.dart
+++ lib/ui/operations_page_widget.dart
@@ -2,10 +2,11 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
-import 'package:scanbot_sdk/json/common_data.dart' as sdk;
 import 'package:scanbot_sdk/cropping_screen_data.dart';
+import 'package:scanbot_sdk/json/common_data.dart' as sdk;
 import 'package:scanbot_sdk/scanbot_sdk.dart';
 import 'package:scanbot_sdk/scanbot_sdk_ui.dart';
+import 'package:scanbot_sdk_example_flutter/ui/classical_components/cropping_custom_ui.dart';
 import 'package:scanbot_sdk_example_flutter/ui/utils.dart';
 
 import '../main.dart';
@@ -78,60 +79,76 @@ class _PageOperationsState extends State<PageOperations> {
               child: Container(
                   padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
                   child: Center(child: pageView))),
-          BottomAppBar(
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    _startCroppingScreen(_page);
-                  },
-                  child: Row(
-                    children: <Widget>[
-                      const Icon(Icons.crop),
-                      Container(width: 4),
-                      const Text(
-                        'Crop & Rotate',
-                        style: TextStyle(inherit: true, color: Colors.black),
-                      ),
-                    ],
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    _showFilterPage(_page);
-                  },
-                  child: Row(
-                    children: <Widget>[
-                      const Icon(Icons.filter),
-                      Container(width: 4),
-                      const Text(
-                        'Filter',
-                        style: TextStyle(inherit: true, color: Colors.black),
-                      ),
-                    ],
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    _deletePage(_page);
-                  },
-                  child: Row(
-                    children: <Widget>[
-                      const Icon(Icons.delete, color: Colors.red),
-                      Container(width: 4),
-                      const Text(
-                        'Delete',
-                        style: TextStyle(inherit: true, color: Colors.red),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
         ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            TextButton(
+              onPressed: () {
+                _startCroppingScreen(_page);
+              },
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const <Widget>[
+                  Icon(Icons.crop),
+                  Text(
+                    'Crop & Rotate',
+                    style: TextStyle(inherit: true, color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                _startCustomUiCroppingScreen(_page);
+              },
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const <Widget>[
+                  Icon(Icons.crop),
+                  Text(
+                    'Crop(custom ui)',
+                    style: TextStyle(inherit: true, color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                _showFilterPage(_page);
+              },
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Icon(Icons.filter),
+                  Container(height: 4),
+                  const Text(
+                    'Filter',
+                    style: TextStyle(inherit: true, color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                _deletePage(_page);
+              },
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Icon(Icons.delete, color: Colors.red),
+                  Container(width: 4),
+                  const Text(
+                    'Delete',
+                    style: TextStyle(inherit: true, color: Colors.red),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -177,6 +194,22 @@ class _PageOperationsState extends State<PageOperations> {
       if (isOperationSuccessful(result) && result.page != null) {
         await _updatePage(result.page!);
       }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _startCustomUiCroppingScreen(sdk.Page page) async {
+    if (!await checkLicenseStatus(context)) {
+      return;
+    }
+
+    try {
+      var newPage = await Navigator.of(context).push(
+        MaterialPageRoute(
+            builder: (context) => CroppingScreenWidget(page: page)),
+      );
+      await _updatePage(newPage!);
     } catch (e) {
       print(e);
     }
