fe437570803b30216465e28470c09e75d6d2f65a
Marko <marko@scanbot.io>
2025-10-17 14:17:14 +0200

    Udpate snippet for mock camera

--- lib/snippets/mock_camera.dart
+++ lib/snippets/mock_camera.dart
@@ -5,7 +5,10 @@ Future mockCamera() async {
   var config = MockCameraParams(imageFileUri: "{path to your image file}");
 
   /**
-   * On Android, the MANAGE_EXTERNAL_STORAGE permission is required, and the image must have even values for both width and height.
+   * For Android:
+   *  API >= 33, READ_MEDIA_IMAGES and READ_MEDIA_VIDEO permissions are required.
+   *  API < 33, READ_EXTERNAL_STORAGE permission is required.
+   *  The image must have even values for both width and height.
    */
   await ScanbotSdk.mockCamera(config);
 }
