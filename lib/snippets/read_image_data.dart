6f1349df030cba87ba42aeae34e379ef97280269
Oleksii <oleksii@scanbot.io>
2026-02-19 18:04:19 +0100

    DS 8.0: Added snippets

---
+++ lib/snippets/read_image_data.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/widgets.dart';
+import 'package:scanbot_sdk/scanbot_sdk.dart';
+
+Future readImageData(String imageFileUri) async {
+  final result = await ScanbotSdk.imageProcessor.readImageData(imageFileUri);
+
+  if (result is Ok<String>) {
+    Uint8List bytes = base64Decode(result.value);
+
+    // use image widget to show preview
+    final image = Image.memory(bytes);
+  }
+}
